a81b0c38fc4c2045f9ae8adfb88355f9f575fa87
NIHAL-TP <nihaltp101@gmail.com>
2026-09-23 21:24:04 +0000

    migrating from ingress to gateway api,set nodeport,changed vcluster values to adapt with gateway,added gatewawy and httproute for routing,updated runner-setup with dependencies ,added nodeport in kind config and update deploy pr env script to adapt to gateway by changing ingress

--- lab/node-test-app/deploy-pr-env.sh
+++ lab/node-test-app/deploy-pr-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 #source .env
-PR_NUMBER=$PR_NUM
+PR_NUMBER=5     #$PR_NUM
 echo "$PR_NUMBER"
 VCLUSTER_NAME="pr-${PR_NUMBER}"
 VCLUSTER_NAMESPACE="vcluster-pr-${PR_NUMBER}"
@@ -41,10 +41,10 @@ echo "pr-${PR_NUMBER} deployment applied successfully."
 kubectl apply -f service.yaml -n $APP_NAMESPACE
 echo "pr-${PR_NUMBER} service applied successfully."
 
-cp ingress.yaml "pr-${PR_NUMBER}-ingress.yaml"
-sed -i "s|host:.*|host: pr-${PR_NUMBER}.local|" "pr-${PR_NUMBER}-ingress.yaml"
-kubectl apply -f pr-${PR_NUMBER}-ingress.yaml -n $APP_NAMESPACE
-echo "pr-${PR_NUMBER} ingress applied"
+cp httproute.yaml "pr-${PR_NUMBER}-httproute.yaml"
+sed -i "s|- pr-.*\.local|- pr-${PR_NUMBER}.local|" "pr-${PR_NUMBER}-httproute.yaml"
+kubectl apply -f pr-${PR_NUMBER}-httproute.yaml -n $APP_NAMESPACE
+echo "pr-${PR_NUMBER} httproute applied"
 
 kubectl wait --for=condition=ready pod -l app=node-test-app -n $APP_NAMESPACE --timeout=120s
 echo "pr-${PR_NUMBER} deployment completed successfully."
--- lab/node-test-app/runner-setup.sh
+++ lab/node-test-app/runner-setup.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 source .env
+
+
+
 #cert manager
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.4/cert-manager.yaml
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --set config.apiVersion="controller.config.cert-manager.io/v1alpha1" \
+  --set config.kind="ControllerConfiguration" \
+  --set config.enableGatewayAPI=true \
+  --set crds.enabled=true
+
+#gateway controller
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.6.1/standard-install.yaml
+kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.7.2" | kubectl apply -f -
+helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric --create-namespace -n nginx-gateway --set nginx.service.type=NodePort
+kubectl wait --timeout=5m -n nginx-gateway deployment/ngf-nginx-gateway-fabric --for=condition=Available
+
+#vcluster
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
+vcluster --version
 #arc
 ARC_NS="github-runner"
 helm install arc \
@@ -27,3 +49,5 @@ helm install "${INSTALLATION_NAME}" \
 --set githubConfigSecret.github_token="${GITHUB_PAT}" \
 oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
 
+kubectl apply -f rbac.yaml
+kubectl apply -f gateway.yaml
